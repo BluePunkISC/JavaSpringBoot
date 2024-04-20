39c5dbea8955b23ae95d3d396f52bc9b740082ad
Ing. Guillermo Espinoza <guillermo.espinoza.isc@gmail.com>
2024-04-19 20:35:59 -0600

    Creación de indice.html, fuera banner de spring desde application.properties, y cambio de REST a MVC desde ControladorREST.java

--- src/main/java/com/lowcostware/practicas/practicas/ControladorREST.java
+++ src/main/java/com/lowcostware/practicas/practicas/ControladorREST.java
@@ -1,17 +1,16 @@
 package com.lowcostware.practicas.practicas;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import lombok.extern.slf4j.Slf4j;
 
-@RestController
+@Controller
 @Slf4j
 public class ControladorREST {
     @GetMapping("/")
     public String comienzo() {
-        log.info("Ejecutando controlador REST");
-        log.info("Más información");
-        return "Proyecto de práctica";
+        log.info("Ejecutando controlador MVC");
+        return "indice";
     }
 }
