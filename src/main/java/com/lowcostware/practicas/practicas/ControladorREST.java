5ad69b63bd6bd17f7dd633e37af846de629d6862
Ing. Guillermo Espinoza <guillermo.espinoza.isc@gmail.com>
2024-04-20 22:34:57 -0600

    Agregación de la clase Model y un par de atributos, mostrar mensajes en el navegador desde application.properties y desde ControladorREST.java

--- src/main/java/com/lowcostware/practicas/practicas/ControladorREST.java
+++ src/main/java/com/lowcostware/practicas/practicas/ControladorREST.java
@@ -1,16 +1,24 @@
 package com.lowcostware.practicas.practicas;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Controller
 @Slf4j
 public class ControladorREST {
+    @Value("${indice.mensaje}")
+    private String mensaje;
+
     @GetMapping("/")
-    public String comienzo() {
+    public String comienzo(Model model) {
+        String hola = "Ejecutando Java Spring Boot";
         log.info("Ejecutando controlador MVC");
+        model.addAttribute("hola", hola);
+        model.addAttribute("mensaje", mensaje);
         return "indice";
     }
 }
